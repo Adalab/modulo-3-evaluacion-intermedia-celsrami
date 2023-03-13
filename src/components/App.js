81ded3be3444a4c4b928146c8eadaa2837457200
celsrami <celsrami22@gmail.com>
2023-03-13 10:31:21 +0100

    New button

--- src/components/App.js
+++ src/components/App.js
@@ -6,46 +6,74 @@ import { useEffect, useState } from "react";
 
 function App() {
   /* VARIABLES Y DATOS */
-  const [data, setData] = useState([]);
-  const [phrase, setPhrase] = useState("");
-  const [character, setcharacter] = useState("all");
+  const [dataFetch, setDataFetch] = useState([]);
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [searchcharacter, setSearchcharacter] = useState("all");
+  const [newPhrase, setNewPhrase] = useState("");
+  const [newCharacter, setNewCharacter] = useState("");
 
   /* EFECTOS */
 
   useEffect(() => {
     callToApi().then((response) => {
-      setData(response);
+      setDataFetch(response);
     });
   }, []);
-
-  console.log(data);
 
   /* FUNCIONES HANDLER */
 
   const handlePhrase = (ev) => {
-    setPhrase(ev.target.value);
+    setSearchPhrase(ev.target.value);
   };
 
   const handleCharacter = (ev) => {
-    setcharacter(ev.target.value);
+    setSearchcharacter(ev.target.value);
+  };
+
+  const handleNewPhrase = (ev) => {
+    setNewPhrase(ev.target.value);
+  };
+
+  const handleNewCharacter = (ev) => {
+    setNewCharacter(ev.target.value);
   };
 
   const handleForm = (ev) => {
     ev.preventDefault();
   };
 
+  //Añadir nueva frase
+
+  const handleButton = () => {
+    const NewObject = { character: newCharacter, quote: newPhrase };
+    const objectAll = [...dataFetch, NewObject];
+    setDataFetch(objectAll);
+  };
   /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
 
   const renderList = () => {
-    return data.map((character, index) => {
-      return (
-        <li key={index}>
-          <p>
-            {character.quote} - <strong>{character.character}</strong>
-          </p>
-        </li>
-      );
-    });
+    return dataFetch
+      .filter((phrase) => {
+        return phrase.quote.toLowerCase().includes(searchPhrase.toLowerCase());
+      })
+      .filter((name) => {
+        if (searchcharacter === "all") {
+          return name;
+        } else {
+          return name.character
+            .toLowerCase()
+            .includes(searchcharacter.toLowerCase());
+        }
+      })
+      .map((character, index) => {
+        return (
+          <li key={index}>
+            <p>
+              {character.quote} - <strong>{character.character}</strong>
+            </p>
+          </li>
+        );
+      });
   };
 
   /* HTML */
@@ -61,15 +89,15 @@ function App() {
             type='name'
             name='phrase'
             id='phrase'
-            value={phrase}
+            value={searchPhrase}
             onChange={handlePhrase}
           />
           <label htmlFor='character'>Filtrar por personaje:</label>
           <select
             id='character'
             name='character'
+            value={searchcharacter}
             onChange={handleCharacter}
-            value={character}
           >
             <option value='all'>Todos</option>
             <option value='ross'>Ross</option>
@@ -79,8 +107,32 @@ function App() {
             <option value='chandler'>Chandler</option>
             <option value='rachel'>Rachel</option>
           </select>
+
+          <ul>{renderList()}</ul>
+          <h2>Añadir una nueva frase</h2>
+          <label htmlFor='newPhrase'>Nueva frase:</label>
+          <input
+            type='name'
+            name='newPhrase'
+            id='newPhrase'
+            value={newPhrase}
+            onChange={handleNewPhrase}
+          />
+          <label htmlFor='newCharacter'>Personaje:</label>
+          <input
+            type='name'
+            name='newCharacter'
+            id='newCharacter'
+            value={newCharacter}
+            onChange={handleNewCharacter}
+          />
+          <input
+            type='submit'
+            name='button'
+            value='Añadir una frase'
+            onClick={handleButton}
+          />
         </form>
-        <ul>{renderList()}</ul>
       </main>
       <footer className='footer'></footer>
     </div>
